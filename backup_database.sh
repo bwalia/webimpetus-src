f03cdff5575c2abe6e452637f0d58ee704869e5a
Harcharan Singh <harcharansingh@Harcharans-MacBook-Pro.local>
2023-10-10 18:27:10 +0530

    added backup database script for dr

---
+++ backup_database.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# MySQL Database Information
+DB_HOST="webimpetus-db"
+DB_USER="wsl_dev"
+DB_PASSWORD="CHANGE_ME"
+DB_NAME="myworkstation_dev"
+
+# Minio Information
+MINIO_HOST="https://s3-cli.workstation.co.uk"
+MINIO_ACCESS_KEY="iwL4MdcmMChRL6r3ptVa"
+MINIO_SECRET_KEY="2HNdx7toyaQJoGCDuS4AKPfCf9F4nsNEkjSQQM20"
+MINIO_BUCKET="mysql-backup"
+
+# Temporary Directory
+TMP_DIR="/tmp"
+
+# Timestamp for the dump file
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+
+# MySQL Dump File Name
+DUMP_FILE="$TMP_DIR/db_dump_$TIMESTAMP.sql"
+
+# Create MySQL Dump
+mysqldump -h$DB_HOST -u$DB_USER -p$DB_PASSWORD $DB_NAME > $DUMP_FILE
+
+if [ $? -eq 0 ]; then
+    echo "MySQL dump created successfully: $DUMP_FILE"
+else
+    echo "Error creating MySQL dump."
+    exit 1
+fi
+
+# Upload Dump File to Minio Bucket
+mc config host add myminio $MINIO_HOST $MINIO_ACCESS_KEY $MINIO_SECRET_KEY
+ls -alh $DUMP_FILE
+file $DUMP_FILE
+stat $DUMP_FILE
+mc cp $DUMP_FILE myminio/$MINIO_BUCKET/
+
+if [ $? -eq 0 ]; then
+    echo "Dump file uploaded to Minio bucket: $MINIO_BUCKET"
+else
+    echo "Error uploading dump file to Minio bucket."
+    exit 1
+fi
+
+# Clean up temporary dump file
+rm $DUMP_FILE
+
+echo "Script completed."
