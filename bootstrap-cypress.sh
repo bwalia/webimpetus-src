a80dd0f92e681c97adce353c0ad38e8837bd2912
Balinder Walia <92169035+bwalia-sciensano@users.noreply.github.com>
2023-12-04 13:48:52 +0100

    Cypress test added

--- bootstrap-cypress.sh
+++ bootstrap-cypress.sh
@@ -5,8 +5,8 @@ set -x
 USERNAME="$1"
 PASSWD="$2"
 
-echo "" > .env.cypress
-echo "CYPRESS_login_username=$USERNAME" >> .env.cypress
-echo "CYPRESS_login_passwd=$PASSWD" >> .env.cypress
-echo "" >> .env.cypress
+echo "" > .env_cypress
+echo "CYPRESS_login_username=$USERNAME" >> .env_cypress
+echo "CYPRESS_login_passwd=$PASSWD" >> .env_cypress
+echo "" >> .env_cypress
 docker-compose -f docker-compose-cypress.yml up -d
