ffacf0c8a1821cb75fe5051064ed8a7edb531339
Balinder Walia <92169035+bwalia-sciensano@users.noreply.github.com>
2023-12-04 10:41:26 +0100

    Cypress login test added

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      chromeWebSecurity: false;
       // implement node event listeners here
+      // pageLoadTimeout: 100000;
     },
   },
 });
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,12 +1,34 @@
+// module.exports = (on, config) => {
+//   on('before:browser:launch', (browser = {}, launchOptions) => {
+//     console.log(launchOptions.args)
+
+//     if (browser.name == 'chrome') {
+//       launchOptions.args.push('--disable-gpu')
+//     }
+
+//     if (browser.name == 'electron') {
+//       launchOptions.args.push('--disable-gpu')
+//     }
+
+//     return launchOptions
+//   }),
+// }
+
 describe('Workstation login test Int environment', () => {
 
   var urlStr = 'https://int-my.workstation.co.uk/#/login'
   urlStr = 'http://localhost:8080/#/login'
+  urlStr = 'http://172.178.0.8:80/#/login'
+  urlStr = 'http://int-my.workstation.co.uk/#/login'
 
   it('passes', () => {
-    cy.visit(urlStr)
-    cy.get('#email').type('admin@admin.com')
-    cy.get('#password').type('$1F0N0qDq%')
+    cy.visit(urlStr).debug()
+
+    var login_username_str = Cypress.env('login_username')
+    var login_password_str = Cypress.env('login_password')
+
+    cy.get('#email').type(login_username_str)
+    cy.get('#password').type(login_password_str)
     cy.get('.btn_1').click()
     cy.get(':nth-child(2) > .single_user_pil > .action_btns > .action_btn > .far').click()
 
