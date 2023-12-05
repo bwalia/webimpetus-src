000401f605e55ea51243e236e23ec614e5b1e530
Harcharan Singh <harcharansingh@Harcharans-MacBook-Pro.local>
2023-12-05 12:00:00 +0530

    Cypress workflow fix

--- bootstrap-cypress.sh
+++ bootstrap-cypress.sh
@@ -5,6 +5,7 @@ set -x
 USERNAME="$1"
 PASSWD="$2"
 
+rm -Rf .env
 echo "" > env_cypress
 echo "CYPRESS_login_username=$USERNAME" >> env_cypress
 echo "CYPRESS_login_passwd=$PASSWD" >> env_cypress
@@ -12,8 +13,7 @@ echo "" >> env_cypress
 mv env_cypress /tmp/.env_cypress
 mv docker-compose-cypress.yml /tmp/docker-compose-cypress.yml
 mv cypress /tmp/cypress
-rm -Rf *env*
-mv /tmp/.env_cypress .env_cypress
+mv /tmp/.env_cypress .env
 mv /tmp/docker-compose-cypress.yml docker-compose-cypress.yml
 mv /tmp/cypress cypress
 ls -al
